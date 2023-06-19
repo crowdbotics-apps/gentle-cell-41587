a03e8af3fa6a023547e9247814c54e89adbdccad
michael cohn <michaelcohn5@gmail.com>
2023-06-18 23:44:15 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "gentle-cell-41587",
   url: "https://gentle-cell-41587.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
